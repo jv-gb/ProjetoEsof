8abb7885c73c3dcbaffa0c2885403f1fa85c798f
jv-gb <joaovgbv@gmail.com>
2024-08-10 00:47:58 -0300

    Protótipo adição de login

--- login/login.js
+++ login/login.js
@@ -1,6 +1,26 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const bodyParser = require('body-parser');
-const app = express();
-const port = 3000;
+const loginFormulario = document.getElementById('login-formulario');
+const userInput = document.getElementById('user-input');
+const passwordInput = document.getElementById('password-input');
+const enviar = document.getElementById('enviar');
+
+// const express = require('express');
+// const path = require('path');
+// const fs = require('fs');
+// const bodyParser = require('body-parser');
+// const app = express();
+// const port = 3000;
+
+const user = 'admin';
+const password = '1234';
+
+enviar.addEventListener('click',()=>{
+    console.log('ola mundo')
+    console.log(userInput.value)
+    console.log(passwordInput.value)
+if(userInput.value == user && passwordInput.value == password){
+
+    location.href = "../principal/adicionar.html";
+}
+
+
+})
